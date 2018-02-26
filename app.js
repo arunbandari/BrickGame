a61b2e306541b23be64ec2c2cab6f3d15b655c3b
arunbandari <arun.bandari@applaudsolutions.com>
2018-02-26 10:36:39 +0530

    New feature - score added

--- app.js
+++ app.js
@@ -13,35 +13,44 @@ var left = 100;
 var up = 100;
 var s_direction = 1;
 var over = false;
+var refreshIntervalId;
+var ispaused = false;
+var score = document.getElementById("score_id");
 var bricks = new Array();
     bricks.push({"left" : 0,  "up" : 10});
-    bricks.push({"left" : 40, "up" : 10});
-    bricks.push({"left" : 80, "up" : 10});
-    bricks.push({"left" : 120, "up" : 10});
-    bricks.push({"left" : 160, "up" : 10});
-    bricks.push({"left" : 200, "up" : 10});
-    bricks.push({"left" : 240, "up" : 10});
+    bricks.push({"left" : 35, "up" : 10});
+    bricks.push({"left" : 70, "up" : 10});
+    bricks.push({"left" : 105, "up" : 10});
+    bricks.push({"left" : 140, "up" : 10});
+    bricks.push({"left" : 175, "up" : 10});
+    bricks.push({"left" : 210, "up" : 10});
+    bricks.push({"left" : 245, "up" : 10});
     bricks.push({"left" : 280, "up" : 10});
-    bricks.push({"left" : 320, "up" : 10});
-    bricks.push({"left" : 360, "up" : 10});
-    bricks.push({"left" : 400, "up" : 10});
-    bricks.push({"left" : 440, "up" : 10});
-    bricks.push({"left" : 0,  "up" : 30});
-    bricks.push({"left" : 40, "up" : 30});
-    bricks.push({"left" : 80, "up" : 30});
-    bricks.push({"left" : 120, "up" : 30});
-    bricks.push({"left" : 160, "up" : 30});
-    bricks.push({"left" : 200, "up" : 30});
-    bricks.push({"left" : 240, "up" : 30});
-    bricks.push({"left" : 280, "up" : 30});
-    bricks.push({"left" : 320, "up" : 30});
-    bricks.push({"left" : 360, "up" : 30});
-    bricks.push({"left" : 400, "up" : 30});
-    bricks.push({"left" : 440, "up" : 30});
 
-var refreshIntervalId = setInterval(function(){
+    bricks.push({"left" : 0,  "up" : 25});
+    bricks.push({"left" : 40, "up" : 25});
+    bricks.push({"left" : 80, "up" : 25});
+    bricks.push({"left" : 120, "up" : 25});
+    bricks.push({"left" : 160, "up" : 25});
+    bricks.push({"left" : 200, "up" : 25});
+    bricks.push({"left" : 240, "up" : 25});
+    bricks.push({"left" : 280, "up" : 25});
+
+    bricks.push({"left" : 0,  "up" : 40});
+    bricks.push({"left" : 35, "up" : 40});
+    bricks.push({"left" : 70, "up" : 40});
+    bricks.push({"left" : 105, "up" : 40});
+    bricks.push({"left" : 140, "up" : 40});
+    bricks.push({"left" : 175, "up" : 40});
+    bricks.push({"left" : 210, "up" : 40});
+    bricks.push({"left" : 245, "up" : 40});
+    bricks.push({"left" : 280, "up" : 40});
+function startGame(){
+	  refreshIntervalId = setInterval(function(){
 	  con.setAttribute("style","left :"+left+"px;top:"+up+"px");
-	  if(Math.abs(left-x)<20&&Math.abs(up-480)<10){
+	  if(left_vary>0) left_vary =1;
+	  else left_vary = -1;
+	  if(Math.abs(left-x)<15&&Math.abs(up-280)<10){
 	  	left_vary = getDirection();
 	  	top_vary = -1;
 	  }
@@ -49,24 +58,27 @@ var refreshIntervalId = setInterval(function(){
 	  	top_vary = 1;
 	  	left_vary = -1 * (left_vary+1)%3;
 	  }
-	  if(left>500||left<0){
+	  if(left>300||left<0){
 	  	left_vary = -1 * left_vary;
 	  }
 	  left = left + left_vary;
 	  up   = up + top_vary;
-	  if(up>490) game_over();
+	  if(up>290) game_over();
 	  for (var i = 0; i < bricks.length; i++) {
 	  	if(bricks[i].left<left&&(bricks[i].left+30)>(left)&&Math.abs(bricks[i].up-up)<20){
 	  		bricks.splice(i,1);
 	  		place_bricks();
 	  		console.log(bricks);
 		  	top_vary = 1;
-		  	left_vary = -1 * left_vary;
+		  	left_vary = getDirection();
+		  	score.innerText = 26 - bricks.length;
 	  		break;
       	}
 	  }
-},20
+},15
 );
+}
+startGame();
 function getDirection(){
 	return Math.round(Math.random() * (3 - (-3) + 1) - 3);
 }
@@ -88,9 +100,17 @@ function place_bricks(){
 }
 place_bricks();
 function dealWithKeyboard(e) {
-	  if(over)
-	  	return;
 	  direction = e.keyCode;
+	  if(direction===32){
+	    ispaused = !ispaused;
+	    if(ispaused){
+		    clearInterval(refreshIntervalId);
+		    refreshIntervalId=null;
+	    }
+	    else
+	    	startGame(); 
+	    return;
+	  }
 	  var bar = document.getElementById("bar");
 	  if(direction===39){
 	  	bar.setAttribute("style","left:"+move(direction)+"px");
@@ -100,13 +120,9 @@ function dealWithKeyboard(e) {
 	  }
 }
 function move(direction){
-	if(direction==39&&x<500)
+	if(direction==39&&x<300)
 		x+=5;
 	else if(x>0)
 		x-=5;
 	return x;
-}
-function getRandomPoint() {
-  return (Math.floor(Math.random() * (385)) + 1);
-
 }
